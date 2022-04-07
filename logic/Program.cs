b64190b25213442130a7e8553bb7afb699407764
kamin0 <jm187@yandex.ru>
2022-04-07 20:22:20 +0300

    Цикл FOR

--- logic/Program.cs
+++ logic/Program.cs
@@ -7,38 +7,40 @@ namespace logic
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-
+			for (int i = 0 ; i < 5 ; i++)
+			{
 			var color = Console.ReadLine();
 
-			switch (color)
-			{
-				case "red":
-					Console.BackgroundColor = ConsoleColor.Red;
-					Console.ForegroundColor = ConsoleColor.Black;
+				switch (color)
+				{
+					case "red":
+						Console.BackgroundColor = ConsoleColor.Red;
+						Console.ForegroundColor = ConsoleColor.Black;
 
-					Console.WriteLine("Your color is red!");
-					break;
+						Console.WriteLine("Your color is red!");
+						break;
 
-				case "green":
-					Console.BackgroundColor = ConsoleColor.Green;
-					Console.ForegroundColor = ConsoleColor.Black;
+					case "green":
+						Console.BackgroundColor = ConsoleColor.Green;
+						Console.ForegroundColor = ConsoleColor.Black;
 
-					Console.WriteLine("Your color is green!");
-					break;
+						Console.WriteLine("Your color is green!");
+						break;
 
-				case "cyan":
-					Console.BackgroundColor = ConsoleColor.Cyan;
-					Console.ForegroundColor = ConsoleColor.Black;
+					case "cyan":
+						Console.BackgroundColor = ConsoleColor.Cyan;
+						Console.ForegroundColor = ConsoleColor.Black;
 
-					Console.WriteLine("Your color is cyan!");
-					break;
+						Console.WriteLine("Your color is cyan!");
+						break;
 
-				default:
-					Console.BackgroundColor = ConsoleColor.Yellow;
-					Console.ForegroundColor = ConsoleColor.Red;
+					default:
+						Console.BackgroundColor = ConsoleColor.Yellow;
+						Console.ForegroundColor = ConsoleColor.Red;
 
-					Console.WriteLine("Your color is default!");
-					break;
+						Console.WriteLine("Your color is default!");
+						break;
+				}
 			}
 		}
 	}
