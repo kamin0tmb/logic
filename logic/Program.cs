007962153fbeae4a4a48abb5a6933f6044242878
kamin0 <jm187@yandex.ru>
2022-04-07 19:11:17 +0300

    Добавьте файлы проекта.

---
+++ logic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "123";
+            string B = "234";
+            var C = (A == B);
+            Console.WriteLine(C);
+        }
+    }
+}
