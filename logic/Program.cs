66c3030fbdcdad81d03f90b3c87f8c8713337c88
kamin0 <jm187@yandex.ru>
2022-04-08 20:48:45 +0300

    4.3.15

--- logic/Program.cs
+++ logic/Program.cs
@@ -6,13 +6,14 @@ namespace logic
 	{
 		static void Main(string[] args)
 		{
-			var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+			var arr = new int[] { -5, 6, -9, 1, 2, -3, 4 };
 			int s = 0;
 			for (int c = 0; c < arr.Length; c++)
             {
-				s += arr[c];
+				if (arr[c] > 0)
+					s++;
             }
-			Console.WriteLine("Сумма всех элементов архива {0}", s);
+			Console.WriteLine("Количество положительных элементов массива равно {0}", s);
 		}
 	}
 }
