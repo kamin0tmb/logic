318ede214a356266993c73e27632c11241339608
kamin0 <jm187@yandex.ru>
2022-04-08 20:38:43 +0300

    4.3.12

--- logic/Program.cs
+++ logic/Program.cs
@@ -6,69 +6,26 @@ namespace logic
 	{
 		static void Main(string[] args)
 		{
-			int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, {11, 12, 13 } };
-			for (int k = 0; k < array.GetUpperBound(1) + 1; k++)
-			{
-				
-					for (int c = 0; c < array.GetUpperBound(0) + 1; c++)
-						Console.Write(array[c, k] + " ");
-
-				Console.WriteLine();
-			}
-			Console.WriteLine();
-			Console.WriteLine("Введите свое имя:");
-			string name = Console.ReadLine();
-			foreach (var ch in name)
-			{
-				Console.Write(ch + " ");
-			}
-			Console.WriteLine("Последняя буква вашего имени {0}", name[name.Length - 1]);
-			int i;
-			Console.WriteLine("Ваше имя наоборот: ");
-			for (i = name.Length - 1; i > -1; i--)
+			var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+			int temp;
+			for (int i = 0; i < arr.Length; i++)
             {
-				Console.Write(name[i] + " ");
-			}
-			Console.WriteLine();
-			Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-			int t = 0;
-			do
-			{
-				Console.WriteLine("Iteration {0}", t);
-				t++;
-				var color = Console.ReadLine();
+				for (int j = i + 1; j < arr.Length; j++)
+                {
+					if (arr[i] > arr[j])
+                    {
+						temp = arr[i];
+						arr[i] = arr[j];
+						arr[j] = temp;
+                    }
 
-				switch (color)
-				{
-					case "red":
-						Console.BackgroundColor = ConsoleColor.Red;
-						Console.ForegroundColor = ConsoleColor.Black;
-
-						Console.WriteLine("Your color is red!");
-						break;
-
-					case "green":
-						Console.BackgroundColor = ConsoleColor.Green;
-						Console.ForegroundColor = ConsoleColor.Black;
-
-						Console.WriteLine("Your color is green!");
-						break;
-
-					case "cyan":
-						Console.BackgroundColor = ConsoleColor.Cyan;
-						Console.ForegroundColor = ConsoleColor.Black;
-
-						Console.WriteLine("Your color is cyan!");
-						break;
-
-					default:
-						Console.BackgroundColor = ConsoleColor.Yellow;
-						Console.ForegroundColor = ConsoleColor.Red;
-
-						Console.WriteLine("Your color is default!");
-						break;
 				}
-			} while (t < 3);
+
+			}
+			for (int c = 0; c < arr.Length; c++)
+            {
+				Console.Write(arr[c]);
+            }
 		}
 	}
 }
