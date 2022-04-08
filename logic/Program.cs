d33cf64635c24f8602b2967a528ff4270376dc8a
kamin0 <jm187@yandex.ru>
2022-04-08 20:41:51 +0300

    4.3.13

--- logic/Program.cs
+++ logic/Program.cs
@@ -7,25 +7,12 @@ namespace logic
 		static void Main(string[] args)
 		{
 			var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-			int temp;
-			for (int i = 0; i < arr.Length; i++)
-            {
-				for (int j = i + 1; j < arr.Length; j++)
-                {
-					if (arr[i] > arr[j])
-                    {
-						temp = arr[i];
-						arr[i] = arr[j];
-						arr[j] = temp;
-                    }
-
-				}
-
-			}
+			int s = 0;
 			for (int c = 0; c < arr.Length; c++)
             {
-				Console.Write(arr[c]);
+				s += arr[c];
             }
+			Console.WriteLine("Сумма всех элементов архива {0}", s);
 		}
 	}
 }
