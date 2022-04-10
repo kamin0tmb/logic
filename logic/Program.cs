ff889597b15961994d21c784c4505fcabb2b4557
kamin0 <jm187@yandex.ru>
2022-04-10 11:38:15 +0300

    4.5.1

--- logic/Program.cs
+++ logic/Program.cs
@@ -6,18 +6,10 @@ namespace logic
 	{
 		static void Main(string[] args)
 		{
-			(string Name, string Type, double Age, int NameCount) Pet;
-			Console.Write("Введите имя Вашего питомца: ");
-			Pet.Name = Console.ReadLine();
-			Console.Write("Введите, какое животное Ваш питомец: ");
-			Pet.Type = Console.ReadLine();
-			Console.Write("Введите возраст Вашего питомца: ");
-			Pet.Age = double.Parse(Console.ReadLine());
-			Pet.NameCount = Pet.Name.Length;
-			Console.WriteLine("Имя питомца: {0}", Pet.Item1);
-			Console.WriteLine("Тип питомца: {0}", Pet.Item2);
-			Console.WriteLine("Возраст питомца: {0}", Pet.Item3);
-			Console.WriteLine("В имени Вашего питомца {0} букв.", Pet.Item4);
+			(string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+		
+
+			
 		}
 	}
 }
