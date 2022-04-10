65cdb949bb1d4c45923d28fa57a385bb7c59380e
kamin0 <jm187@yandex.ru>
2022-04-10 10:49:45 +0300

    4.4.2

--- logic/Program.cs
+++ logic/Program.cs
@@ -6,25 +6,15 @@ namespace logic
 	{
 		static void Main(string[] args)
 		{
-			int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
-			int temp;
-			for (int c = 0; c <= arr.GetUpperBound(0); c++)
-			{
-				for (int i = 0; i <= arr.GetUpperBound(1); i++)
-				{
-					for (int j = i + 1; j <= arr.GetUpperBound(1); j++)
-					{
-						if (arr[c,i] > arr[c,j])
-						{
-							temp = arr[c,j];
-							arr[c,j] = arr[c,i];
-							arr[c,i] = temp;
-						}
-					}
-					Console.Write(arr[c,i] + " ");
-				}
-				Console.WriteLine();	
-			}
+
+			(string name, int age) anketa;
+			Console.WriteLine("Введите Ваше имя: ");
+			anketa.name = Console.ReadLine();
+			Console.WriteLine("Введите Ваш возраст: ");
+			anketa.age = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Ваше имя: {0}", anketa.Item1);
+			Console.WriteLine("Ваш возраст: {0}", anketa.Item2);
+
 		}
 	}
 }
