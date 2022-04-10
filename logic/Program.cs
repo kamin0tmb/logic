4957aec9a2504f4fcf441c2a16037e65e0f00834
kamin0 <jm187@yandex.ru>
2022-04-10 11:38:37 +0300

    4.5.2

--- logic/Program.cs
+++ logic/Program.cs
@@ -7,9 +7,15 @@ namespace logic
 		static void Main(string[] args)
 		{
 			(string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
-		
-
+			Console.Write("Введите имя: ");
+			User.Name = Console.ReadLine();
+			Console.Write("Введите фамилию: ");
+			User.LastName = Console.ReadLine();
+			Console.Write("Введите логин: ");
+			User.Login = Console.ReadLine();
 			
+
+
 		}
 	}
 }
